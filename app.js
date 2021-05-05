93a33014048a25cb2cbc26ad0158762d1c25df49
anujin626 <anu64310@gmail.com>
2021-05-06 07:42:54 +0800

    event listeneruudiig uusgej ogloo

---
+++ app.js
@@ -0,0 +1,27 @@
+// Дэлгэцтэй ажиллах контроллер
+var uiController = (function() {
+  
+  })();
+
+  //санхүүтэй ажиллах
+  var financeController = (function() {})();
+
+  //холбогч контроллер
+  var appController = (function(uiController, financeController) {
+    var ctrlAddItem = function() {
+        //1.орлуулах өгөгдлийг дэлгэцээс олж авах
+        console.log("дэлгэцээс өгөгдлөө авах хэсэг");
+        //2.олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална
+        //3.олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана 
+        //4.төсвийг тооцно
+        //5.эцсийн үлдэглэл
+    };
+    document.querySelector(".add__btn").addEventListener("click", function() {
+      ctrlAddItem();
+    });
+    document.addEventListener("keypress", function(event) {
+      if (event.keyCode === 13) {
+        ctrlAddItem();
+      }
+    } );
+  })(uiController, financeController);
